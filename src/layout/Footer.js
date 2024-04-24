545c493033885807202bede3c6ed4fdf9e8cf9ad
Isac Vitor <isac24vitor@gmail.com>
2024-04-24 19:11:59 -0300

    first commit

---
+++ src/layout/Footer.js
@@ -0,0 +1,10 @@
+import Styles from './NavBar.module.css'
+function Footer(){
+    return(
+        <footer className={Styles.navbar}>
+            <h3>WADRE</h3>
+            <h3>YURY</h3>
+        </footer>
+    )
+}
+export default Footer
